4bd22d360a113f104da8efe198d00536e0e1cadf
NeranjiSulakshika <vpnsulakshika@students.nsbm.lk>
2021-01-09 22:23:55 +0530

    updated

--- WEB Assignment - Agriculture/Report.aspx.cs
+++ WEB Assignment - Agriculture/Report.aspx.cs
@@ -35,7 +35,6 @@ namespace WEB_Assignment___Agriculture
             {
                 CalendarMFG.Visible = true;
             }
-
             CalendarMFG.Attributes.Add("style", "position:absolute");
         }
 
@@ -62,21 +61,6 @@ namespace WEB_Assignment___Agriculture
         {
             txtMFG.Text = CalendarMFG.SelectedDate.ToString("dd/MM/yyyy");
             CalendarMFG.Visible = false;
-        }
-
-        protected void ImageUpload_Click(object sender, EventArgs e)
-        {
-            string folderPath = Server.MapPath("~/Images/");
-
-            //Check whether Directory (Folder) exists.
-            if (!Directory.Exists(folderPath))
-            {
-                //If Directory (Folder) does not exists Create it.
-                Directory.CreateDirectory(folderPath);
-            }
-
-            //Save the File to the Directory (Folder).
-            ImageUpload.SaveAs(folderPath + Path.GetFileName(ImageUpload.FileName));
         }
 
         protected void btnSubmit_Click(object sender, EventArgs e)
@@ -109,91 +93,6 @@ namespace WEB_Assignment___Agriculture
                     txtMFG.Text = "";
                     txtEXP.Text = "";
                 }
-
-
-                    
-
-                    /*
-                    string[] validFileTypes = { "bmp", "gif", "png", "jpg", "jpeg", "doc", "xls" };
-                    string ext = System.IO.Path.GetExtension(ImageUpload.PostedFile.FileName);
-                    bool isValidFile = false;
-
-                    for (int i = 0; i < validFileTypes.Length; i++)
-                    {
-                        if (ext == "." + validFileTypes[i])
-
-                        {
-                            isValidFile = true;
-                            break;
-                        }
-                    }
-                    SqlConnection con = new SqlConnection("server=127.0.0.1;user id=root;database=doa");
-                    SqlCommand cmd = new SqlCommand(@"INSERT INTO reports (F_NIC, F_House_Name, F_Crop_Type, F_Image, F_Crop_MFG, F_Crop_EXP, F_Address, Latitude, Longitude) VALUES ('" + txtNIC.Text + "' , '" + txtName.Text + "' , '" + txtCropType.Text + "' , '" + ext + "' , '" + txtMFG.Text + "' , '" + txtEXP.Text + "' , '" + "" + "' , '" + "" + "' , '" + "" + "')", con);
-                    con.Open();
-                    cmd.Connection = con;
-                    cmd.ExecuteNonQuery();
-                    con.Close(); */
-
-
-                    /*
-                    //take virtual path to store in the database
-                    string path = "~/Images/" + ImageUpload.FileName;
-
-                    SqlConnection con = new SqlConnection("Data Source=DESKTOP-VHPDJKD;Initial Catalog=DoA;Integrated Security=True");
-                    SqlCommand cmd = new SqlCommand(@"INSERT INTO Reports (F_NIC, F_House_Name, F_Crop_Type, F_Image, F_Crop_MFG, F_Crop_EXP, F_Location, Latitude, Longitude) VALUES ('" + txtNIC.Text + "' , '" + txtName.Text + "' , '" + txtCropType.Text + "' , '" + path + "' , '" + txtMFG.Text + "' , '" + txtEXP.Text + "' , '" + "" + "' , '" + txtLatitude.Text + "' , '" + txtLongitude.Text + "')", con);
-
-                    //store image in folder image. to get the absolute path we use Server.MapPath
-
-                    ImageUpload.SaveAs(Server.MapPath("Images") + "/" + ImageUpload.FileName);
-                    con.Open();
-                    //pass connection and query to your command object
-                    cmd.Connection = con;
-                    cmd.ExecuteNonQuery();
-                    con.Close();*/
-                    /*
-                                        if (ImageUpload.HasFile)
-                                        {
-                                            int imagefilelenth = ImageUpload.PostedFile.ContentLength;
-                                            byte[] imgarray = new byte[imagefilelenth];
-                                            HttpPostedFile image = ImageUpload.PostedFile;
-                                            image.InputStream.Read(imgarray, 0, imagefilelenth);
-
-                                            SqlConnection con = new SqlConnection("Data Source=DESKTOP-VHPDJKD;Initial Catalog=DoA;Integrated Security=True");
-                                            SqlCommand cmd = new SqlCommand(@"INSERT INTO Reports (F_NIC, F_House_Name, F_Crop_Type, F_Image, F_Crop_MFG, F_Crop_EXP, F_Location, Latitude, Longitude) VALUES ('" + txtNIC.Text + "' , '" + txtName.Text + "' , '" + txtCropType.Text + "' , @Image , '" + txtMFG.Text + "' , '" + txtEXP.Text + "' , '" + "" + "' , '" + txtLatitude.Text + "' , '" + txtLongitude.Text + "')", con);
-                                            con.Open();
-                                            cmd.Parameters.AddWithValue("@Image", SqlDbType.Image).Value = imgarray;
-                                            cmd.ExecuteNonQuery();
-                                            lblSuccess.Visible = true;
-
-                                            txtNIC.Text = "";
-                                            txtName.Text = "";
-                                            txtCropType.Text = "";
-                                            txtMFG.Text = "";
-                                            txtEXP.Text = "";
-                                        }*/
-                    /*
-                    if (ImageUpload.HasFile)
-                    {
-                        string str = ImageUpload.FileName;
-                        ImageUpload.PostedFile.SaveAs(Server.MapPath("~/Images/" + "/" + str));
-                        string imgpath = "~/Images/" + "/" + str.ToString();
-
-                        SqlConnection con = new SqlConnection("Data Source=DESKTOP-VHPDJKD;Initial Catalog=DoA;Integrated Security=True");
-
-                        SqlCommand cmd = new SqlCommand(@"INSERT INTO Reports (F_NIC, F_House_Name, F_Crop_Type, F_Image, F_Crop_MFG, F_Crop_EXP, F_Location, Latitude, Longitude) VALUES ('" + txtNIC.Text + "' , '" + txtName.Text + "' , '" + txtCropType.Text + "' ,  @image   , '" + txtMFG.Text + "' , '" + txtEXP.Text + "' , '" + "" + "' , '" + txtLatitude.Text + "' , '" + txtLongitude.Text + "')", con);
-                        cmd.Parameters.AddWithValue("@image", imgpath);
-                        con.Open();
-                        cmd.ExecuteNonQuery();
-                        con.Close();
-                        lblSuccess.Visible = true;
-
-                        txtNIC.Text = "";
-                        txtName.Text = "";
-                        txtCropType.Text = "";
-                        txtMFG.Text = "";
-                        txtEXP.Text = "";
-                    }*/
-                
                 catch (Exception ex)
                 {
                     Response.Write("Error!" + ex);
