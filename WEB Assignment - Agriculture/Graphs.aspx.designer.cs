82ebba131057420a06ab509b31c7c0d74ce37f02
pdssigera <67218599+pdssigera@users.noreply.github.com>
2021-01-12 19:46:50 +0530

    update

--- WEB Assignment - Agriculture/Graphs.aspx.designer.cs
+++ WEB Assignment - Agriculture/Graphs.aspx.designer.cs
@@ -13,14 +13,5 @@ namespace WEB_Assignment___Agriculture
 
     public partial class Graphs
     {
-
-        /// <summary>
-        /// form1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
     }
 }
